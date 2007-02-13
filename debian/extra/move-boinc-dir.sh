46a2681fc8adc6ca4eaaae7a0a0144a731448fb2
fst-guest <fst-guest@2db7d633-71b8-0310-b1c1-c0b6732fd93e>
2007-02-13 07:31:39 +0000

    * The wxWidgets patch is not needed anymore. * Replaced the last occurances of log_flags.xml with cc_config.xml.

--- debian/extra/move-boinc-dir.sh
+++ debian/extra/move-boinc-dir.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-# This script can be used to move an BOINC data directory to Debian's
+# This script can be used to move a BOINC data directory to Debian's
 # default location of the BOINC data directory /var/lib/boinc-client.
 # Invoke it with the old data directory as it's only argument,
 # for example:
 #    % move-boinc-dir.sh /home/john/BOINC
 #
-# Copyright © 2006 Debian BOINC Maintainers
-#                  <pkg-boinc-devel@lists.alioth.debian.org>
+# Copyright © 2006-2007 Debian BOINC Maintainers
+#                       <pkg-boinc-devel@lists.alioth.debian.org>
 #
 # This file is licensed under the terms of the GNU General Public License,
 # Version 2 or any later version published by the Free Software Foundation.
@@ -19,7 +19,7 @@ if [ -z "$OLD_DIR" ]; then
     echo "Error: Old BOINC directory not specified."
     exit 1
 elif [ ! -d "$OLD_DIR" ]; then
-    echo "Error: $OLD_DIR does not exists or is not a directory."
+    echo "Error: $OLD_DIR does not exist or is not a directory."
     exit 1
 fi
 
@@ -32,7 +32,6 @@ mv_conffile()
         mv -f "$BOINC_DIR/$1" "$CONF_DIR/$1"
     fi
     ln -sf "$CONF_DIR/$1" "$BOINC_DIR/$1"
-    chown boinc:boinc "$BOINC_DIR/$1" || true
 }
 
 # Stop the BOINC core client.
@@ -48,9 +47,9 @@ chown -R boinc:boinc $BOINC_DIR || true
 
 # Move old configuration files to /etc/boinc-client/ and
 # create symlinks for the BOINC core client.
+mv_conffile cc_config.xml
 mv_conffile global_prefs_override.xml
 mv_conffile gui_rpc_auth.cfg
-mv_conffile log_flags.xml
 mv_conffile remote_hosts.cfg
 
 CA_FILE=/etc/ssl/certs/ca-certificates.crt
