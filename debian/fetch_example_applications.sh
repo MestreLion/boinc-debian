f736fbd0f22ea7e8ff6f86322350ec3afd8fb66d
Dhananjay M Balan <mb.dhananjay@gmail.com>
2011-07-11 00:24:31 +0530

    FIX: Last Commit 1. Directory structure created was wrong. 2. Changed the version string. 3. Collection directory not cleaned by default.

--- debian/fetch_example_applications.sh
+++ debian/fetch_example_applications.sh
@@ -3,12 +3,7 @@
 # This script shall support the initial setup 
 # of a BOINC server by assisting in the download
 # of example applications from Debian.
-# 
-# This script downloads the binaries, puts them in a 
-# direcotory structure as BOINC 
-# wants them( see http://boinc.berkeley.edu/trac/wiki/AppVersionNew ), 
-# and then signs them.
-# 
+#
 # This script is released under the same license
 # as BOINC, created and copyright by
 # Steffen Moeller <moeller@debian.org>
@@ -28,54 +23,71 @@ deb2boinc[sparc64]="sparc64-linux-gnu"
 deb2boinc[mips]="mips-linux-gnu"
 deb2boinc[s390]="s390-linux-gnu"
 
-# Path to your projects key file and sign_executable binary.
-keyfile="/home/dhananjay/projects/test/keys/code_sign_private"
-signapp=/home/dhananjay/projects/test/bin/sign_executable
-
 mirror="http://ftp.de.debian.org/debian"
-version="6.12.33+dfsg-1"
-shortver=$(echo $version|cut -d . -f1-2)
+version="6.12.28+dfsg-2"
+
+projectroot=""
+
+sign=$projectroot"/bin/sign_executable"
+key=$projectroot"/keys/code_sign_private"
+
+shortver=$(echo $version|cut -d . -f-2)
 
 for arch in ${!deb2boinc[@]}
 do
 
-    if [ ! -r $arch.deb ]; then 
-	url="$mirror/pool/main/b/boinc/boinc-app-examples_${version}_${arch}.deb"
-	if ! wget -O - $url > ${arch}.deb ; then
-	    echo "Platform '$arch' failed to download .... skipping."
-	    rm -f ${arch}.deb
-	    continue
-	fi
-    fi
+  if [ -d collection/$arch ]; then
+    echo "Destination directory 'collection/arch' already exiting ... skipping."
+    continue
+  fi
 
-    ar xvf ${arch}.deb data.tar.gz
-    echo "Untaring"
-    tar xzf data.tar.gz ./usr/lib/boinc-server/apps/
-    echo "Contents:"
-    ls ./usr/lib/boinc-server/apps/
-    mkdir -p collection
-    mv ./usr/lib/boinc-server/apps collection/$arch
-    mv usr deleteThisDir
-    rm -r deleteThisDir
-    
-    echo Reorganising and signing the application binaries.
-    
-    for f in collection/$arch/*
-    do
-    	appname=`echo $f|cut -d / -f 3`
-	apppath=apps/$appname/$shortver/${deb2boinc[$arch]}/
-	mkdir -p  $apppath
-	mv $f $apppath
-	${signapp} $apppath/$appname $keyfile >> $apppath/${appname}.sig
-    done
-    
-    
+  if [ ! -r $arch.deb ]; then 
+    url="$mirror/pool/main/b/boinc/boinc-app-examples_${version}_${arch}.deb"
+    if ! wget -O - $url > ${arch}.deb ; then
+       echo "Platform '$arch' failed to download .... skipping."
+       rm -f ${arch}.deb
+       continue
+    fi
+  fi
+
+  ar xvf ${arch}.deb data.tar.gz
+  echo "Untaring"
+  tar xzf data.tar.gz ./usr/lib/boinc-server/apps/
+  echo "Contents:"
+  ls ./usr/lib/boinc-server/apps/
+  mkdir -p collection
+  mv ./usr/lib/boinc-server/apps collection/$arch
+  mv usr deleteThisDir
+  rm -r deleteThisDir
 done
 
-echo cleaning up.
-
-if [ -d collection ]
+if [ -d apps ]
 then
-    rm -r collection
+    echo App directory already exists, Exiting!!
+    exit
 fi
+
+echo Creating directories
+for f in collection/$arch/*
+do
+    appname=`echo $f|cut -d / -f3`
+    mkdir -p apps/$appname
+done
+
+for app in apps/*
+do 
+    appname=$(echo $app|cut -d / -f 2)
+    for folder in collection/*
+    do 
+	archname=$(echo $folder|cut -d / -f2)
+	echo Copying $folder/${appname} apps/$appname/${appname}_${shortver}_${deb2boinc[$archname]}
+	cp $folder/${appname} apps/$appname/${appname}_${shortver}_${deb2boinc[$archname]}
+    done
+done
+
+for binary in apps/*/*
+do
+    echo Signing $binary
+    $sign $binary $key >> ${binary}.sig
+done
 
